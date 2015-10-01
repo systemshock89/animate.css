9c9c213a1d2e80d8d8291f6026f58540e3c24318
systemshock89 <system.shock89@gmail.com>
2015-10-01 14:46:36 +0300

    first commit

---
+++ js/app/common.js
@@ -0,0 +1,72 @@
+﻿$(function(){
+
+    /* Верхнее меню */
+    var navLi = $('.menu-top ul > li > a');
+
+    // Выделение активного пункта при скролле
+    $('.block')
+        .waypoint( function (dir) {
+            var hash = $(this).attr('id');
+
+            if (dir === "down") { //скролл сверху вниз
+                menuItemSelect(navLi, hash);
+            }
+        }, {
+            offset: $('header').height()
+        })
+        .waypoint( function (dir) {
+            var hash = $(this).attr('id');
+
+            if (dir === "up") { //скролл снизу вверх
+                menuItemSelect(navLi, hash);
+            }
+        }, {
+            offset: function() {
+                return - $(this).outerHeight() + $('header').height();
+            }
+        });
+
+    //ф-я подставляет активному пункту класс selected
+    function menuItemSelect(navLi, hash){
+        navLi.parent().removeClass('selected');
+
+        $.each( navLi, function() {
+            if ( $(this).attr('href').slice(1) === hash ){
+                $(this).parent().addClass('selected');
+            }
+        });
+    }
+
+    //плавный скролл
+    navLi.click(function (e) {
+        e.preventDefault();
+
+        var elementClick = $(this).attr("href"),
+            destination = $(elementClick).offset().top,
+            offset = $('header').height();
+
+        $('html, body').stop().animate( { scrollTop: destination - offset }, 800 );
+    });
+    /* /Верхнее меню */
+
+
+    /* скролл хедера с position:fixed при маленьком размере окна */
+    $(document).scroll(function() {
+        $( "header" ).css('left', -$(document).scrollLeft());
+    });
+    /* /скролл хедера с position:fixed при маленьком размере окна */
+
+
+    /* Прилипающий элемент */
+    if( $(".sticky-element").is("div") ){
+        $.getScript( '/js/lib/waypoints-sticky.min.js', function() {
+
+            $('.sticky-element').waypoint('sticky', {
+                offset: $('header').height()
+            });
+
+        });
+    }
+    /* /Прилипающий элемент */
+
+}); // END READY
